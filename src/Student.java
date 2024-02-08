3bbdb6b8499d90b84f71f46b08f93e9af0d9ab68
RithuReddy <74108081+RithuReddy@users.noreply.github.com>
2024-02-08 12:21:52 -0500

    new

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public void method(){
+        System.out.println("hello world");
+    }
+
+    public static void main(String args[]){
+        Student s = new Student();
+        s.method();
+    }
+}
